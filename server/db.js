f489ecb66f52bd94d2302c26d9691c9cffd4c97f
tesuforo <javiergarciaruiz1989@gmail.com>
2023-03-31 18:38:29 -0500

    javier garcia 31-032023

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ import { createPool } from 'mysql2/promise'
 
 
 export const pool = createPool({
-    host: '5.183.11.6',
+    host: '127.0.0.1',
     port: 3306,
     user: 'root',
     password :'89javier',
