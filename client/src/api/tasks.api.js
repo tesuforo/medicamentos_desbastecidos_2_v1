f08df859393ce2ce1fac21fb0cfef5e4ca4bee1c
tesuforo <javiergarciaruiz1989@gmail.com>
2023-03-31 18:33:43 -0500

    javier garcia 31-032023

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,24 +2,24 @@ import axios from 'axios'
 
 
 export const getTasksRequest = async () =>
-await axios.get("http://localhost:4001/tasks");
+await axios.get("http://5.183.11.6:4001/tasks");
 
 
  export const createTaskRequest = async (task) =>
-  await axios.post("http://localhost:4001/tasks", task);
+  await axios.post("http://5.183.11.6:4001/tasks", task);
 
 
   export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://localhost:4001/tasks/${id}`);
+  await axios.delete(`http://5.183.11.6:4001/tasks/${id}`);
 
   export const getTaskRequest = async (id) =>
-  await axios.get(`http://localhost:4001/tasks/${id}`);
+  await axios.get(`http://5.183.11.6:4001/tasks/${id}`);
 
  
   export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:4001/tasks/${id}`, newFields);
+  await axios.put(`http://5.183.11.6:4001/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`http://localhost:4001/tasks/${id}`, {
+  await axios.put(`http://5.183.11.6:4001/tasks/${id}`, {
     done,
   });
