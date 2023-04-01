368cdea1663dcc09ae7b396e67acdee2395d9f94
tesuforo <javiergarciaruiz1989@gmail.com>
2023-03-31 19:50:02 -0500

    javier garcia 31-032023

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import {dirname, join} from 'path'
-import {fileURLToPath} from 'url'
+
 import { PORT } from "./config.js";
 
 import indexRoutes from "./routes/index.routes.js";
@@ -9,16 +8,15 @@ import taskRoutes from "./routes/tasks.routes.js";
 
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname)
 
-app.use(cors({ origin: 'http://5.183.11.6:5173'}));
+
+app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
 app.use(taskRoutes);
 
-app.use(express.static(join(__dirname, '../client/dist')))
+
 
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
