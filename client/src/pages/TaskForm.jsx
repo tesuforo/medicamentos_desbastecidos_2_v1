036cc0b76a899c639ff9d594457757f8185b2bb3
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-03 21:39:15 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -70,11 +70,8 @@ function TaskForm() {
 
 
 
-       <div className="flex flex-wrap justify-center">
-      <div className="w-6/12 sm:w-4/12 px-16">
-    <img src={formula1} alt="..." className="shadow-lg rounded max-w-full h-auto align-middle border-none" />
-          </div>
-             </div>
+    <img class="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800" src={formula1} alt="image description">
+
 
       <Formik
         initialValues={task}
