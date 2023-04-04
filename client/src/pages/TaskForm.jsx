3ed5317942b6e19c90c9cae6f6b11d35207830d6
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-03 21:01:04 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -64,7 +64,7 @@ function TaskForm() {
       <h3 className="text-3xl font-bold underline"> {params.id ?  "Revisar Solicitudes " : "Bienvenido" }  </h3>
 
      <div className="flex flex-wrap justify-center">
-<div className="w-6/12 sm:w-4/12 px-8">
+<div className="w-6/12 sm:w-4/12 px-2">
   <img src={formula1} alt="..." className="shadow-lg rounded max-w-full h-auto align-middle border-none" />
 </div>
 </div>
