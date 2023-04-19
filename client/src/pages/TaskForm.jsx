59b7c0f168028209c208bcf8d4c8f14df4288c57
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-19 00:28:14 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -71,7 +71,7 @@ function TaskForm() {
     
 
        <div className="flex flex-wrap justify-center">
-  <div className="w-64 sm:w-0/0 px-0">
+  <div class="h-auto max-w-full">
     <img src={formula1} alt="..." className="shadow-lg rounded max-w-full h-auto align-middle border-none" />
             </div>
             </div>
