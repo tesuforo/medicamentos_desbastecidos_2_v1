69e3816506738f5d18e212a3eb8b33828c2529f1
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-18 23:01:47 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -121,14 +121,14 @@ function TaskForm() {
 
 
             <label className="block" htmlFor="departamento">Departamento*</label>
-            <select name="departamento" onChange={handleChange} value={values.departamento}class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <select name="departamento" onChange={handleChange} value={values.departamento} class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             <option value="">Seleccione</option>
               <option value="Cundinamarca">Cundinamarca</option>
 
             </select>
 
             <label className="block" htmlFor="title">Sede*</label>
-            <select name="title" onChange={handleChange} value={values.title}class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <select name="title" onChange={handleChange} value={values.title} class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
               <option value="">Seleccione</option>
               <option value="Bienestar Soacha">Bienestar Soacha</option>
 
