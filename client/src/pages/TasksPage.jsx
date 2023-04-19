fe264a6dd39b9694b53c0204f90879511b13ba39
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-18 22:22:32 -0500

    javier garcia 31-032023

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -17,6 +17,9 @@ function TasksPage() {
   }
   return (
     <div >
+        <div>
+
+    </div>
 
     <div class="hidden sm:mb-8 sm:flex sm:justify-center">
         <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -34,6 +37,7 @@ function TasksPage() {
            <a href="/Linea_de_frente" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Ingreso a Linea de frente <span aria-hidden="true">&rarr;</span></a>
         </div>
       </div>
+
       {rederMain()}
     </div>
   );
