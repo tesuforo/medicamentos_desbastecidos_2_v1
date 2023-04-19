11281bb68371a20339f86913b819d7955ddbf4f8
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-19 00:46:45 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -120,9 +120,7 @@ function TaskForm() {
               {params.id ? "Editar Solicitud" : ""}
             </h1>
 
-            <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8"/>
-
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Formulario</h2>
+    <h1 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Formulario</h1>
   
 
             <label className="block" htmlFor="departamento">Departamento*</label>
