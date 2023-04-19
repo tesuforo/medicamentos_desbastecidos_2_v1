28398e7e8e74fe95f853461989f56c31410924a8
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-19 11:39:40 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -340,7 +340,19 @@ function TaskForm() {
               value={values.descripciones}
              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
 
-
+               <div class="flex h-6 items-center">
+       
+          <button type="button" class="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="true" aria-labelledby="switch-1-label">
+            <span class="sr-only">Acepta las Politicas de Privacidad</span>
+          
+            <span aria-hidden="true" class="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
+          </button>
+        </div>
+        <label class="text-sm leading-6 text-gray-600" id="switch-1-label">
+        Al seleccionar esto, usted acepta nuestra
+          <a href="#" class="font-semibold text-indigo-600">Privacida&nbsp;Politica</a>.
+        </label>
+        
 
             < button class="rounded-full"   
             type="submit" 
