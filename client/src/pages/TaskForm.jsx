863dd90d706d3d4e729ab173a1610f22709ec86b
tesuforo <javiergarciaruiz1989@gmail.com>
2023-04-20 15:07:09 -0500

    javier garcia 31-032023

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -345,9 +345,11 @@ function TaskForm() {
 
   
  
-  <input type="checkbox" value="1" class="sr-only peer"/>
+  <label class="relative inline-flex items-center mb-5 cursor-pointer">
+  <input type="checkbox" value={values.done} class="sr-only peer"/>
   <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
   <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Acepta</span>
+</label>
 
         
         <label class="text-sm leading-6 text-gray-600" id="switch-1-label">
